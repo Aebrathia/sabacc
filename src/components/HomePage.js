38c09662da99f0dbfc44d2340c0f6e0d9606249a
Andrea Maugars <andrea.maugars@gmail.com>
2019-05-19 19:31:25 +0300

    Wrap homepage in a div for convention

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,13 +11,15 @@ const HomePage = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username</label>
-            <input id="username" name="username" type="text" />
-            <label htmlFor="game">Game</label>
-            <input id="game" name="game" type="text" />
-            <button type="submit">Join or create a game</button>
-        </form>
+        <div className="HomePage">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input id="username" name="username" type="text" />
+                <label htmlFor="game">Game</label>
+                <input id="game" name="game" type="text" />
+                <button type="submit">Join or create a game</button>
+            </form>
+        </div>
     );
 };
 
